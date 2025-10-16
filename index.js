95366253951be6de8be656353dbd4b81b312580e
Yadira Zeballos <yadira@tecsup.edu.pe>
2025-10-16 09:49:16 -0500

    Corregido puerto

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 const http = require('http')
 
 function requesController(req, res) {
-  console.log('Bienvenidos al curso')
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Bienvenidos al curso desde Render 🚀')
+    console.log('Bienvenidos al curso')
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Bienvenidos al curso')
 }
 
 const PORT = process.env.PORT || 4000
 
 const server = http.createServer(requesController)
 
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Aplicación corriendo en: ${PORT}`)
+server.listen(PORT, '0.0.0.0', function() {
+    console.log("Aplicacion corriendo en: " + PORT)
 })
