6c90b176b23dc86cc6ad44d0bb62b8df23d450c8
Yadira Zeballos <yadira@tecsup.edu.pe>
2025-10-16 09:42:18 -0500

    Corregido puerto dinámico para Render

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const http = require('http')
 
-function requesController(){
-    console.log('Bienvenidos al curso')
+function requesController(req, res) {
+  console.log('Bienvenidos al curso')
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Bienvenidos al curso desde Render 🚀')
 }
+
+const PORT = process.env.PORT || 4000
 
 const server = http.createServer(requesController)
 
-const PORT=process.env.PORT
-server.listen(PORT, function(){
-    console.log("Aplicacion corriendo en: " + PORT)
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Aplicación corriendo en: ${PORT}`)
 })
