b2b2b34e98af2efa03aec597d1bd3b827a09ea45
Yadira Zeballos <yadira@tecsup.edu.pe>
2025-10-16 09:55:18 -0500

    Agregado contenido visible para Render

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 require('dotenv').config()
 const http = require('http')
 
-function requesController(req, res) {
+function requesController(req, res){
     console.log('Bienvenidos al curso')
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Bienvenidos al curso')
+    res.end('<h1>Bienvenidos al curso</h1>')
 }
-
-const PORT = process.env.PORT || 4000
 
 const server = http.createServer(requesController)
 
-server.listen(PORT, '0.0.0.0', function() {
+const PORT = process.env.PORT || 4000
+server.listen(PORT, '0.0.0.0', function(){
     console.log("Aplicacion corriendo en: " + PORT)
 })
